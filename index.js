f449a5b3181f0019a0fa6e7b300bd7ffbf3754e0
tharunadhithya28 <tharun2839@gmail.com>
2023-06-15 05:25:07 +0000

    JSX

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="background-color">
+    <h1 className="heading"> Super Over League </h1>
+    <div className="image-container">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      ></img>
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
